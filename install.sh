802476cb5e6efbd1ae51f7a65e898f611ae44d6b
amith7319 <36619176+amith7319@users.noreply.github.com>
2020-12-25 23:14:31 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,3 +12,20 @@ if [ ! -x /usr/sbin/nginx ];
     else
         systemctl restart nginx && echo "NGINX is INSTALLED and RUNNING"
 fi
+yum -y install mariadb mariadb-server
+systemctl start mariadb && systemctl enable mariadb
+VERSION=`mysql -V |awk '{print $5}' |sed "s/-[[:alpha:]].*$//"`
+if [ -z "`mysql -V |grep -i mariadb`" ]; then # There is MySQL server
+ if [[ "$VERSION" > "5.6.9" ]]; then 
+    NEW=1
+    else
+    NEW=0
+ fi
+else # There is MariaDB server 10.4
+ if [[ "$VERSION" > "10.4" ]]; then 
+    NEW=1
+    else
+    NEW=0
+    yum -y upgrade 
+ fi
+fi
