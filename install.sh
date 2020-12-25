19babf04fcef56d319d1deee9195eff3ac0e34cf
amith7319 <36619176+amith7319@users.noreply.github.com>
2020-12-26 00:09:37 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,21 +11,37 @@ if [ ! -x /usr/sbin/nginx ];
     else
         systemctl restart nginx && echo "NGINX is INSTALLED and RUNNING"
 fi
+
+## Installing Mariadb
+
 yum -y install mariadb mariadb-server
 systemctl start mariadb && systemctl enable mariadb
 VERSION=`mysql -V |awk '{print $5}' |sed "s/-[[:alpha:]].*$//"`
-if [ -z "`mysql -V |grep -i mariadb`" ]; then # There is MySQL server
- if [[ "$VERSION" > "5.6.9" ]]; then 
-    echo "MYSQL is NEWEST
-    else
-    echo "MYSQL need to upgraded
-    yum -y upgrade
- fi
-else # There is MariaDB server 10.4
- if [[ "$VERSION" > "10.4" ]]; then 
-    echo "MARIADB is NEWEST
-    else
-    echo "MARIADB need to upgraded
-    yum -y upgrade 
- fi
+if [ -z "`mysql -V |grep -i mariadb`" ]; then
+   echo MYSQL VERSION IS $VERSION
 fi
+password=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 20 | head -n 1)
+echo Mysql root password = $password > /root/LEMPpassword.txt
+mysql_secure_installation <<EOF
+
+y
+$password
+$password
+y
+n
+y
+y
+EOF
+
+## Installing PHP
+
+yum install http://rpms.remirepo.net/enterprise/remi-release-7.rpm -y
+yum --disablerepo="*" --enablerepo="remi-safe" list php[7-9][0-9].x86_64 |grep php
+echo #############################################################################
+read -p "Select one PHP version from above...like php70,php71,php80  " phpv
+echo #############################################################################
+yum-config-manager --enable remi-$phpv
+yum install php php-common php-mbstring php-gd php-intl php-xml php-json php-mysqlnd php-fpm -y
+
+
+
